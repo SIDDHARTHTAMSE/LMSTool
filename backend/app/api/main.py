e4d393b598b3973838c671a0b1643dde23f7250e
Siddarth Tamse <sidtamse7@gmail.com>
2024-08-23 16:32:04 +0530

    add contactus router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils,signup
+from app.api.routes import items, login, users, utils, signup, contactus
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -8,3 +8,4 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(signup.router, prefix="/signup", tags=["signup"])
+api_router.include_router(contactus.router, prefix="/contactus", tags=["contactus"])
