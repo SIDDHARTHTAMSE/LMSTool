0c9da04d765a5d0bf450e347b3be54895d60d10d
Siddarth Tamse <sidtamse7@gmail.com>
2024-08-29 13:26:35 +0530

    add get_user_profile, delete_user_profile and update_user_profile api's

--- backend/app/api/routes/user_profile.py
+++ backend/app/api/routes/user_profile.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.crud import user_profile_crud
-from app.schemas.signup import CreateSignUp, CreateSignUpRes, to_signup_res
+from app.schemas.signup import CreateSignUp, CreateSignUpRes, to_signup_res, UpdateUserProfile
 from app.schemas.signin import SigninRequest
 from app.api.deps import SessionDep
 from fastapi import status
@@ -59,3 +59,60 @@ def sign_in(session: SessionDep, signin_req: SigninRequest):
         status_code=status.HTTP_200_OK,
         content="Successfully signed in"
     )
+
+
+@router.get("{email_id}", response_model=CreateSignUpRes)
+def get_user_profile(session: SessionDep, email_id: str):
+    existing_email = user_profile_crud.get_user_profile_by_email(session=session, email=email_id)
+
+    if not existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User email id not found"
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=to_signup_res(existing_email)
+    )
+
+
+@router.delete("{email_id}")
+def delete_user_profile(session: SessionDep, email_id: str):
+    user_to_delete = user_profile_crud.get_user_profile_by_email(session=session, email=email_id)
+
+    if not user_to_delete:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User email not found"
+        )
+
+    user_profile_crud.delete_by_user(session=session, user=user_to_delete)
+    return JSONResponse(
+        content="User profile deleted successfully"
+    )
+
+
+@router.put("/email_id", response_model=CreateSignUpRes)
+def update_existing_user_profile(session: SessionDep, email_id: str, user_req: UpdateUserProfile):
+    existing_email = user_profile_crud.get_user_profile_by_email(session=session, email=email_id)
+
+    if not existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User email id not found"
+        )
+
+    existing_email.full_name = user_req.full_name or existing_email.full_name
+    existing_email.first_name = user_req.first_name or existing_email.first_name
+    existing_email.last_name = user_req.last_name or existing_email.last_name
+    existing_email.email = user_req.email or existing_email.email
+    existing_email.phone_number = user_req.phone_number or existing_email.phone_number
+    existing_email.password = user_req.password or existing_email.password
+
+    existing_email = user_profile_crud.update_user_profile(session=session, user=existing_email)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=to_signup_res(existing_email)
+    )
+
