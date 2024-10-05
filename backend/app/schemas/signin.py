89337ef11a2c324916ff66c24b224b9eba969b5d
Siddarth Tamse <sidtamse7@gmail.com>
2024-10-06 02:45:13 +0530

    added full_name attribute in SigninRequest, SigninResponse and to_signin_res

--- backend/app/schemas/signin.py
+++ backend/app/schemas/signin.py
@@ -1,20 +1,24 @@
 from pydantic import BaseModel
-
+from typing import Optional
+from app.schemas.signup import CreateSignUpRes
 from app.models import UserProfile
 
 
 class SigninRequest(BaseModel):
+    full_name: Optional[str] = None
     email: str
     password: str
 
 
 class SigninResponse(BaseModel):
+    full_name: str
     email: str
     password: str
 
 
 def to_signin_res(signin: UserProfile):
     return SigninResponse(
+        full_name=signin.full_name,
         email=signin.email,
         password=signin.password
     ).dict()
